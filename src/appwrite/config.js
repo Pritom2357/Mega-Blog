35ddf288b69cf561bf8301f749f89292fcb01e9d
Pritom2357 <pritombiswas9999@gmail.com>
2024-07-31 20:43:16 +0600

    first commit

---
+++ src/appwrite/config.js
@@ -0,0 +1,143 @@
+import conf from "../conf/conf";
+import {Client, ID, Databases, Storage, Query} from "appwrite";
+
+export class Service{
+    client = new Client();
+    databases;
+    bucket;
+
+    constructor(){
+        this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectID);
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client);
+    }
+
+    async createPost({title, slug, content, featuredImage, status, userId}){
+        try {
+            return await this.databases.createDocument(
+                conf.appwriteDatabaseID,
+                conf.appwriteCollectionID,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite service::createPost::error", error);
+        }
+    }
+
+    async updatePost(slug, {title, content, featuredImage, status}){
+        try {
+            return await this.updateDocument(
+                conf.appwriteDatabaseID,
+                conf.appwriteCollectionID,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite service::updatePost::error", error);
+        }
+    }
+
+    async deletePost(slug){
+        try {
+            await this.databases
+            .deleteDocument(
+                conf.appwriteDatabaseID,
+                conf.appwriteCollectionID,
+                slug
+            )
+            return true;
+        } catch (error) {
+            console.log("Appwrite service::deletePost::error", error);
+            return false;
+        }
+    }
+
+    async getPost(slug){
+        try {
+            return await this.databases
+            .getDocument(
+                conf.appwriteDatabaseID,
+                conf.appwriteCollectionID,
+                slug
+            )
+        } catch (error) {
+            console.log("Appwrite service::getPost::error", error);
+            return false;
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status", "active")]){
+        try {
+            return await this.databases
+            .listDocuments(
+                conf.appwriteDatabaseID,
+                conf.appwriteCollectionID,
+                queries
+            )
+        } catch (error) {
+            console.log("Appwrite service::getPosts::error", error);
+            return false;
+        }
+    }
+
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketID,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log("Appwrite service::uploadFile::error", error);
+            return false;
+        }
+    }
+
+    async deleteFile(fileID){
+        try {
+            await this.bucket.deleteFile(
+                conf.appwriteBucketID,
+                fileID
+            );
+            return true;
+        } catch (error) {
+            console.log("Appwrite service::deleteFile::error", error);
+            return false;
+        }
+    }
+
+    getFilePreview(fileID){
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketID,
+            fileID
+        )
+    }
+
+    downloadFile(fileID, fileName){
+        const downloadUrl = `${conf.appwriteUrl}/storage/files/${fileID}/view?project=${conf.appwriteProjectID}`;
+
+        const link = document.createElement('a');
+        link.href = downloadUrl;
+        link.download = fileName;
+        link.target = '_blank';
+        link.click();
+    }
+};
+
+const service = new Service();
+
+export default service;
---
+++ src/components/index.js
@@ -0,0 +1,32 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import Logoutbutton from "./Header/Logoutbutton";
+import Button from './Button';
+import RTE from './RTE';
+import Postcard from './Postcard';
+import Signup from "./Signup";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm";
+// import Post from "./Post";
+import AuthLayout from "./AuthLayout";
+import Input from "./Input"
+import Select from "./Select";
+
+export{
+    Header,
+    Footer,
+    Container,
+    Logo,
+    Logoutbutton,
+    Button,
+    RTE,
+    Signup,
+    Postcard,
+    Login,
+    PostForm,
+    AuthLayout,
+    Input,
+    Select,
+}
